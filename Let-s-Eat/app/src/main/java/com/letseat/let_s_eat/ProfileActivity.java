4a634b1b0fd729c5729f2cf2f7d6eee0d7315626
Jamin Goo <jgoo@utexas.edu>
2017-02-05 00:42:31 -0600

    Activities Setup

---
+++ Let-s-Eat/app/src/main/java/com/letseat/let_s_eat/ProfileActivity.java
@@ -0,0 +1,13 @@
+package com.letseat.let_s_eat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
