e9eb05f49c22ecc4430a92cc280e50a8c331556c
Jamin Goo <jgoo@utexas.edu>
2017-04-21 23:22:35 -0500

    implemented parcelable

--- Let-s-Eat/app/src/main/java/com/letseat/let_s_eat/SearchActivity.java
+++ Let-s-Eat/app/src/main/java/com/letseat/let_s_eat/SearchActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -18,39 +17,57 @@ public class SearchActivity extends Activity implements SearchView.OnQueryTextLi
     ListView listView;
     ListViewAdapter adapter;
     SearchView editSearch;
+    ArrayList<TestPlace> arrayPlaces;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         listView = (ListView) findViewById(R.id.view_searchList);
-        ArrayList<TestPlace> arrayName = new ArrayList<TestPlace>();
+        //arraylist to hold data
+        arrayPlaces = new ArrayList<TestPlace>();
+        //load the sample data created my myself
         for(String name : getResources().getStringArray(R.array.array_name)){
-         arrayName.add(new TestPlace(name));
+         arrayPlaces.add(new TestPlace(name));
         }
-        adapter = new ListViewAdapter(this, arrayName);
+        //now convert the data into items on the listview
+        adapter = new ListViewAdapter(this, arrayPlaces);
         listView.setAdapter(adapter);
+        //implementing click listener on listview items and query
         listView.setOnItemClickListener(SearchActivity.this);
         editSearch = (SearchView)findViewById(R.id.view_search);
         editSearch.setOnQueryTextListener(this);
-
     }
 
     @Override
+    /*
+    method called when the user submits the query
+     */
     public boolean onQueryTextSubmit(String query) {
         return false;
     }
 
+    /*
+    called when the query text is changed by the user.
+     */
     @Override
     public boolean onQueryTextChange(String newText) {
+        //update the query text
         String text = newText;
+        //now update the list using the method implemented in listview adapter
         adapter.filter(text);
         return false;
     }
-
+    /*
+    method called when an item on the list is clicked
+    it launches the search activity as well as passing the information of the place
+     */
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String selected = ((TextView) view.findViewById(R.id.name)).getText().toString();
+        //toast to make sure that I am getting the right object
+        //String selected = ((TextView) view.findViewById(R.id.place)).getText().toString();
+        TestPlace data = adapter.getItem(position);
+        String selected = data.getName();
         Toast toast = Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_SHORT);
         toast.show();
         Intent intent = new Intent(SearchActivity.this, ProfileActivity.class);
